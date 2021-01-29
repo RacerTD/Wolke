baa72ac092eb65e5c769cc198e5f37cb45eea379
Tobias <tobiasdeyle@googlemail.com>
2021-01-29 11:18:20 +0100

    Stuff

--- Wolke/Assets/Scripts/PlayerScripts/PlayerController.cs
+++ Wolke/Assets/Scripts/PlayerScripts/PlayerController.cs
@@ -2,10 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.VFX;
 
 public class PlayerController : AbilityController
 {
     [SerializeField] private Transform cameraTransform;
+    [SerializeField] private Camera controlledCamera;
+    [SerializeField] [Tooltip("Particles per second")] private float shootRate = 100f;
+    private InputAction.CallbackContext shootInputAction;
+    [SerializeField] private float particleSpread = 10f;
+    [SerializeField] private LayerMask particleLayerMask;
+    [SerializeField] private VisualEffect singleParticleSystem;
 
     protected override void Start()
     {
@@ -14,12 +21,46 @@ public class PlayerController : AbilityController
 
     protected override void Update()
     {
+        base.Update();
+    }
 
+    private void FixedUpdate()
+    {
+        HandleParticleShoot();
     }
 
     public void HandleViewInput(InputAction.CallbackContext context)
     {
 
+    }
+
+    public void HandleParticleShootInput(InputAction.CallbackContext context)
+    {
+        shootInputAction = context;
+    }
+
+    private void HandleParticleShoot()
+    {
+        if ((shootInputAction.performed || shootInputAction.started) && singleParticleSystem != null)
+        {
+            //Debug.Log(shootRate * Time.deltaTime);
+            for (int i = 0; i < shootRate * Time.fixedDeltaTime; i++)
+            {
+                RaycastHit[] hits = Physics.RaycastAll(cameraTransform.position, GenerateShootDirection(), 100f, particleLayerMask);
+
+                if (hits.Length > 0)
+                {
+                    //Debug.Log(hits[0].point);
+                    singleParticleSystem.SetVector3("ParticlePos", hits[0].point);
+                    singleParticleSystem.SendEvent("CreateParticle");
+                }
+            }
+        }
+    }
+
+    private Vector3 GenerateShootDirection()
+    {
+        return ((cameraTransform.position + cameraTransform.forward * 1000).normalized) + new Vector3(Random.Range(particleSpread, -particleSpread) / 100, Random.Range(particleSpread, -particleSpread) / 100, Random.Range(particleSpread, -particleSpread) / 100);
     }
 
     /// <summary>
